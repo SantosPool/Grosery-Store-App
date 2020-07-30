72a787fd51f8e736769f32b47c3eea56de47ca8c
SantosPool <santos.pool.nahuat@gmail.com>
2020-07-30 14:17:17 -0500

    Se agrega comentario sobre nuget genfu para azure devops CI

--- TiendaServicios.Api.libro.Tests/LibrosServiceTest.cs
+++ TiendaServicios.Api.libro.Tests/LibrosServiceTest.cs
@@ -18,6 +18,7 @@ namespace TiendaServicios.Api.libro.Tests
     {
         private IEnumerable<LibreriaMaterial> ObtenerDataPrueba()
         {
+            //este metodo es para llenar con data de genfu
             A.Configure<LibreriaMaterial>()
                 .Fill(x => x.Titulo).AsArticleTitle()
                 .Fill(x => x.LibreriaMaterialId, () => { return Guid.NewGuid(); });
